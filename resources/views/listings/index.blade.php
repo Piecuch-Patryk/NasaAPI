17be335912ae2bbbdfa9fdecd2f7c801193b28c3
Patryk <patrykowo@gmail.com>
2022-10-03 17:36:20 +0200

    Add bootstrap.

---
+++ resources/views/listings/index.blade.php
@@ -0,0 +1,27 @@
+@extends('welcome')
+
+@section('header')
+    <header class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Choose one API and have fun!</h1>
+            </div>
+        </div>
+    </header>
+@endsection
+
+@section('content')
+    <main class="container">
+        <ul class="row">
+            @foreach ($listings as $listing)
+            <li class="col-12">
+                <h5 class="fs-4">{{$listing->name}}</h5>
+                <p>{{$listing->description}}</p>
+                <a href="#" class="btn btn-sm btn-info">More</a>
+            </li>
+            @endforeach
+        </ul>
+
+    </main>
+@endsection
+
